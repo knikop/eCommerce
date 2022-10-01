1214cc47bf892566cfb574a3877916056c765457
SaqibAhmadSyed <74795435+SaqibSyed-prog@users.noreply.github.com>
2022-09-30 20:09:22 -0400

    Add files via upload
    need to test database + working on user controller (almost done)

---
+++ app/core/controllers/LoginController.php
@@ -0,0 +1,15 @@
+<?php
+class LoginController extends \app\core\Controller {
+    public function index() {
+
+    }
+
+    public function register() {
+
+    }
+
+    public function logout() {
+        
+    }
+}
+?>
--- app/core/init.php
+++ app/core/init.php
@@ -1,2 +1,6 @@
 <?php
-    require("app/core/autoload.php");
+    session_start();
+    require_once("app/core/autoload.php");
+    require_once("app/core/Controller.php");
+    require_once("app/core/Model.php")    
+?>
