04b00dd2e52e2649e56f7bac637a9797ff238117
knikop <2003knikop@gmail.com>
2022-10-02 18:52:46 -0400

    some changes

--- app/core/init.php
+++ app/core/init.php
@@ -1,6 +1,6 @@
 <?php
     session_start();
     require_once("app/core/autoload.php");
-    require_once("app/core/Controller.php");
-    require_once("app/core/Model.php")    
+    //require_once("app/core/Controller.php");
+    //require_once("app/core/Model.php")    
 ?>
