a54ff22fb63248b29e72eb1f915a747c2759f2dd
knikop <2003knikop@gmail.com>
2022-10-10 19:16:03 -0400

    changed the quotations

--- app/models/Profile.php
+++ app/models/Profile.php
@@ -4,7 +4,7 @@ namespace app\models;
 class Profile extends \app\core\Model {
 
     public function get($profile_id) {
-        $SQL = 'SELECT * FROM profile WHERE profile_id = :profile_id';
+        $SQL = "SELECT * FROM profile WHERE profile_id = :profile_id";
         $STMT = self::$_connection->prepare($SQL);
         $STMT->execute(['profile_id'=> $profile_id]);
         $STMT->setFetchMode(\PDO::FETCH_CLASS, "app\models\Profile");
@@ -12,7 +12,7 @@ class Profile extends \app\core\Model {
     }
 
     public function getAll() {
-        $SQL = 'SELECT * FROM profile';
+        $SQL = "SELECT * FROM profile";
         $STMT = self::$_connection->prepare($SQL);
         $STMT->execute();
         $STMT->setFetchMode(\PDO::FETCH_CLASS, "app\models\Profile");
@@ -20,16 +20,16 @@ class Profile extends \app\core\Model {
     }
 
     function insert($user_id) { 
-        $SQL = 'INSERT INTO profile(user_id, first_name, middle_name, last_name) 
-        VALUES(:user_id, :first_name, :middle_name, :last_name)';
+        $SQL = "INSERT INTO profile(user_id, first_name, middle_name, last_name) 
+        VALUES(:user_id, :first_name, :middle_name, :last_name)";
         $STMT = self::$_connection->prepare($SQL);
         $STMT->execute(['user_id'=>$user_id, 'first_name'=>$this->first_name, 
         'middle_name'=>$this->middle_name, 'last_name'=>$this->last_name]);
     }
 
     function update() {
-        $SQL = 'UPDATE profile SET first_name = :first_name, middle_name = :middle_name,
-         last_name = :last_name WHERE profile_id = :profile_id';
+        $SQL = "UPDATE profile SET first_name = :first_name, middle_name = :middle_name,
+         last_name = :last_name WHERE profile_id = :profile_id";
         $STMT = self::$_connection->prepare($SQL);
         $STMT->execute(['first_name'=>$this->first_name, 'middle_name'=>$this->middle_name, 
         'last_name'=>$this->last_name, 'profile_id'=>$this->profile_id]);
