6fbbca4e46295354bc45971bebf4ffbb841c28ca
knikop <2003knikop@gmail.com>
2022-10-10 19:12:55 -0400

    added Profile controller and model

---
+++ app/controllers/Profile.php
@@ -0,0 +1,12 @@
+<?php
+namespace app\controllers;
+
+class Profile extends \app\core\Controller {
+
+    public function index($profile_id) {
+        $profile = new \app\models\Profile();
+        $cp = $profile->get($profile_id);
+        $this->view('Profile/profile', $cp);
+    }
+    
+}
---
+++ app/models/Profile.php
@@ -0,0 +1,38 @@
+<?php
+namespace app\models;
+
+class Profile extends \app\core\Model {
+
+    public function get($profile_id) {
+        $SQL = 'SELECT * FROM profile WHERE profile_id = :profile_id';
+        $STMT = self::$_connection->prepare($SQL);
+        $STMT->execute(['profile_id'=> $profile_id]);
+        $STMT->setFetchMode(\PDO::FETCH_CLASS, "app\models\Profile");
+        return $STMT->fetch();
+    }
+
+    public function getAll() {
+        $SQL = 'SELECT * FROM profile';
+        $STMT = self::$_connection->prepare($SQL);
+        $STMT->execute();
+        $STMT->setFetchMode(\PDO::FETCH_CLASS, "app\models\Profile");
+        return $STMT->fetchAll();
+    }
+
+    function insert($user_id) { 
+        $SQL = 'INSERT INTO profile(user_id, first_name, middle_name, last_name) 
+        VALUES(:user_id, :first_name, :middle_name, :last_name)';
+        $STMT = self::$_connection->prepare($SQL);
+        $STMT->execute(['user_id'=>$user_id, 'first_name'=>$this->first_name, 
+        'middle_name'=>$this->middle_name, 'last_name'=>$this->last_name]);
+    }
+
+    function update() {
+        $SQL = 'UPDATE profile SET first_name = :first_name, middle_name = :middle_name,
+         last_name = :last_name WHERE profile_id = :profile_id';
+        $STMT = self::$_connection->prepare($SQL);
+        $STMT->execute(['first_name'=>$this->first_name, 'middle_name'=>$this->middle_name, 
+        'last_name'=>$this->last_name, 'profile_id'=>$this->profile_id]);
+    }
+
+}
